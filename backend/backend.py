64ed04d7f13c85562ebdddc638530186b295b761
RDDT-Websites <118314380+RDDT-Websites@users.noreply.github.com>
2022-12-06 13:02:47 +0000

    added navbar changes & more

--- backend/backend.py
+++ backend/backend.py
@@ -1,28 +1,7 @@
 import tornado.web
 import tornado.ioloop
 from webassets import Environment
+import com
 
+com.sub()
 
-class MainHandler(tornado.web.RequestHandler):
-    def initialize(self, assets):
-        self.assets = assets
-
-    def get(self):
-        self.render("pages/index.html", assets=assets)
-
-
-# Set up the webassets environment and make a bundle
-assets = Environment( url="/static")
-js_files = ["index.js"]
-assets.register("bundle", *js_files,
-                output="bundled.min.js",
-                filters="rjsmin")  # webassets ships with this filter included
-
-app = tornado.web.Application(
-    [(r'/', MainHandler, dict(assets=assets))],
-    static_path=".",
-    template_path=".",
-    debug=True)
-
-
-tornado.ioloop.IOLoop.current().start()
--- index.js
+++ index.js
@@ -8,6 +8,25 @@ app.use(express.static(path.join(__dirname, "/static/")))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "/pages/index.html"))
 })
+app.get('/weather', (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/weather/forecast.html"))
+})
+app.get('/hourly-forecast', (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/weather/hourly.html"))
+})
+app.get('/misc-forecast', (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/weather/misc.html"))
+})
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/about.html"))
+})
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/contact.html"))
+})
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, "/pages/login.html"))
+})
+
 
 app.listen(port, () => {
   console.log(`Cloud weather listening on port ${port}!`)

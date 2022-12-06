03bfab2b5cd2dc50f705f9083e2b46cd9aa914dd
RDDT-Websites <118314380+RDDT-Websites@users.noreply.github.com>
2022-12-06 00:22:10 +0000

    Backend started, navbar almost done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "/static/")))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, "/pages/index.html"))
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Cloud weather listening on port ${port}!`)
 })
